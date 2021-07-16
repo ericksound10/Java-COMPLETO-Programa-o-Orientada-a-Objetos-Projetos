d968e3922dec4f1ba6a359e13faae096e4720599
ericksound10 <ericksound10@gmail.com>
2021-07-16 18:10:03 -0300

    Anotação casting

---
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Casting/teste_Casting.java
@@ -0,0 +1,13 @@
+public class teste_Casting {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; //coloca-se o double para fazer a conversão do tipo inteiro para double.
+
+        System.out.println(resultado);
+    }
+}
