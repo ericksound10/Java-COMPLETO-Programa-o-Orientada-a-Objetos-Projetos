7f14d9fd0a092ae85af97bdba663cfb91296428f
ericksound10 <ericksound10@gmail.com>
2021-07-17 18:11:36 -0300

    Primeiro_programa

--- Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Primeiro_programa/Primeiro_programa.java
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Primeiro_programa/Primeiro_programa.java
@@ -1,6 +1,6 @@
 public class Primeiro_programa {
     public static void main(String[] args) {
-        System.out.println("Olá mundo"); //Primeiro programa em Java feito no VS Code.
+        System.out.println("Olá mundo"); //Primeiro programa em Java feito no VS Code..
     }
     
 }
