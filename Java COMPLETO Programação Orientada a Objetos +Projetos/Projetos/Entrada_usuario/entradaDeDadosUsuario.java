d68f6a0926bcd8031a7f4eff5185a871ef2011c6
ericksound10 <ericksound10@gmail.com>
2021-07-17 18:50:05 -0300

    Ler número inteiro que o usuário digitou.

--- Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Entrada_usuario/entradaDeDadosUsuario.java
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Entrada_usuario/entradaDeDadosUsuario.java
@@ -12,6 +12,10 @@ public class entradaDeDadosUsuario {
         x = sc.next(); //sc.next serve para ler String
         System.out.println("Você digitou: " + x);
 
+        int t;
+        t = sc.nextInt(); // nextInt serve para ler um número inteiro que o usuário digitou
+        System.out.println("Você digitou um número inteiro: " + t);
+
         sc.close();
     }
     
