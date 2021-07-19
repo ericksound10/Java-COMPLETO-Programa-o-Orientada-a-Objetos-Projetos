69f877c4b67b445a43efa67e3b549d1bd7389dfa
ericksound10 <ericksound10@gmail.com>
2021-07-19 10:32:08 -0300

    Estrutura Condicional IF ELSE

---
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Aulas/Estrutura Condicional/estrutura_Condicional_Simples_if.java
@@ -0,0 +1,17 @@
+public class estrutura_Condicional_Simples_if {
+    public static void main(String[] args) {
+        int x = 5;
+
+        System.out.println("Bom dia");
+
+        if (x > 0){
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+
+
+    }
+
+    
+}
