0ffb8847d1397d8fda448414e0703c2bfaa35206
Erickson Fernandes <50972579+ericksound10@users.noreply.github.com>
2021-07-19 09:44:58 -0300

    Delete Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos directory
    Ajuste nome para Aulas, pois não são projetos ainda.

--- Java COMPLETO Programação Orientada a Objetos +Projetos/Projetos/Primeiro_programa/Primeiro_programa.java
+++
@@ -1,6 +0,0 @@
-public class Primeiro_programa {
-    public static void main(String[] args) {
-        System.out.println("Olá mundo"); //Primeiro programa em Java feito no VS Code.
-    }
-    
-}
